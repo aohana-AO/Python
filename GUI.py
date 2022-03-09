998f3bc293752ac4017395da0209d22981562536
aohana-AO <kbc21a34@stu.kawahara.ac.jp>
2022-03-09 22:40:28 +0900

    tkinter2

---
+++ GUI.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter.constants import BOTTOM
+
+BOTTOM = [
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3'],
+    ['00', '0', '=']
+]
+SYMBOL = ['1', '2', '3', '4', '5']
